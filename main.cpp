8c2776de5f1099a704d58b490658d334105defe0
ciec20151104680 <cxxxe4680@Mac28.local>
2016-10-31 21:09:59 +0800

    1031

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1017
+//
+//  Created by cxxxe4680 on 2016/10/17.
+//  Copyright © 2016年 cxxxe4680. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
